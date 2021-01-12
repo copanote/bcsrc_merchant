8697f736c8cfc07f377892a8d6c11ffe6805eeba
Dongwook Shin <copanote@gmail.com>
2021-01-12 16:49:48 +0900

    react page

--- src/main/java/com/merchant/demo/MerchantController.java
+++ src/main/java/com/merchant/demo/MerchantController.java
@@ -1,11 +1,19 @@
 package com.merchant.demo;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,7 +37,7 @@ public class MerchantController {
 
 	@RequestMapping("/home")
 	public String home() {
-		return "HelloWorld";
+		return "static/test.html";
 	}
 	
 	@Controller
@@ -44,7 +52,7 @@ public class MerchantController {
 	    @GetMapping(value = "/welcome2", produces = MediaType.TEXT_HTML_VALUE)
 	    @ResponseBody
 	    public String welcomeAsHTML2() throws IOException {
-		    String blogUrl = "http://localhost:7070/welcome";
+		    String blogUrl = "static/vn/index.html";
 		    Document doc = Jsoup.connect(blogUrl).get();
 		    
 		    Element link = new Element(Tag.valueOf("a"), "")
@@ -56,7 +64,40 @@ public class MerchantController {
 	        return doc.toString();
 	    }
 	    
+	    @GetMapping(value = "/vn/welcome3", produces = MediaType.TEXT_HTML_VALUE)
+	    @ResponseBody
+	    public String welcomeAsHTML3() throws IOException {
+	    	Document doc = null;
+	    	ClassPathResource resource = new ClassPathResource("static/vn/index.html");
+	    	System.out.println("resource" + resource.toString());
+	    	try {
+	    	    Path path = Paths.get(resource.getURI());
+	    	    byte[] content = Files.readAllBytes(path);
+	    	    String s = new String(content, "utf-8");
+	    	    System.out.println("contents " + s);
+			    doc = Jsoup.parse(s);
+			    
+			    Element link = new Element(Tag.valueOf("a"), "")
+			    		  .text("Checkout this amazing website!")
+			    		  .attr("href", "http://baeldung.com")
+			    		  .attr("target", "_blank");
+			    doc.appendChild(link);
+	    	} catch (IOException e) {
+	    		e.printStackTrace();
+	    	}
+	    	return doc.toString(); 
+
+	    }
 	    
+	    @RequestMapping("/page")
+	    public String someOtherPage(HttpServletRequest request, HttpServletResponse response) {
+	        return "forward:/static/vn/index.html"; 
+	    }
+	    
+		@RequestMapping("/home22")
+		public String home() {
+			return "forward:/static/test.html";
+		}
 
 	    
 	}
