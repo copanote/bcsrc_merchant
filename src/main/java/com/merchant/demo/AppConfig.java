efa8cc32df72a305bc00e06d9f0a10f712aeab47
DONGWOOK SHIN <copanote@gmail.com>
2021-05-06 22:33:13 +0900

    jsoup

---
+++ src/main/java/com/merchant/demo/AppConfig.java
@@ -0,0 +1,34 @@
+package com.merchant.demo;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		return mapper;
+	}
+
+	@Bean("vnSrci")
+	public String vietnamSrci() throws IOException {
+		ClassPathResource resource = new ClassPathResource("static/vn/index.html");
+		Path p = Paths.get(resource.getURI());
+		byte[] b = Files.readAllBytes(p);
+		return new String(b, Charset.forName("utf-8"));
+	}
+
+}
--- src/main/java/com/merchant/demo/MerchantController.java
+++ src/main/java/com/merchant/demo/MerchantController.java
@@ -1,11 +1,8 @@
 package com.merchant.demo;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,7 +10,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +20,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MerchantController {
 
+	@Resource(name = "vnSrci")
+	private String vnHtml;
+
 	@RequestMapping(value = "/merchant")
 	public ModelAndView merchant() {
 		return new ModelAndView("merchant");
@@ -33,75 +32,87 @@ public class MerchantController {
 	public ModelAndView test() {
 		return new ModelAndView("test");
 	}
-	
 
 	@RequestMapping("/home")
 	public String home() {
 		return "static/test.html";
 	}
-	
+
 	@Controller
 	public class HtmlController {
-	    @GetMapping(value = "/welcome", produces = MediaType.TEXT_HTML_VALUE)
-	    @ResponseBody
-	    public String welcomeAsHTML() {
-	        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
-	          "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
-	    }
-	    
-	    @GetMapping(value = "/welcome2", produces = MediaType.TEXT_HTML_VALUE)
-	    @ResponseBody
-	    public String welcomeAsHTML2() throws IOException {
-		    String blogUrl = "static/vn/index.html";
-		    Document doc = Jsoup.connect(blogUrl).get();
-		    
-		    Element link = new Element(Tag.valueOf("a"), "")
-		    		  .text("Checkout this amazing website!")
-		    		  .attr("href", "http://baeldung.com")
-		    		  .attr("target", "_blank");
-		    doc.appendChild(link);
-		   
-	        return doc.toString();
-	    }
-	    
-	    @GetMapping(value = "/vn/welcome3", produces = MediaType.TEXT_HTML_VALUE)
-	    @ResponseBody
-	    public String welcomeAsHTML3() throws IOException {
-	    	
-	    	//TODO:: URL 정책  
-	    	Document doc = null;
-	    	ClassPathResource resource = new ClassPathResource("static/vn/index.html");
-	    	System.out.println("resource" + resource.toString());
-	    	try {
-	    	    Path path = Paths.get(resource.getURI());
-	    	    byte[] content = Files.readAllBytes(path);
-	    	    String s = new String(content, "utf-8");
-	    	    System.out.println("contents " + s);
-			    doc = Jsoup.parse(s);
-			    
-			    Element link = new Element(Tag.valueOf("script"), "")
-			    		  .text("window.__VP__ = { srcClientId: \"43a83638-0122-4a9f-baaa-a4b9eb3caa51\",srcDpaId: \"c21195dc-39eb-422c-8328-db2b0b535ed3\"}");
+		@GetMapping(value = "/welcome", produces = MediaType.TEXT_HTML_VALUE)
+		@ResponseBody
+		public String welcomeAsHTML() {
+			return "<html>\n" + "<header><title>Welcome</title></header>\n" + "<body>\n" + "Hello world\n" + "</body>\n"
+					+ "</html>";
+		}
+
+		@GetMapping(value = "/welcome2", produces = MediaType.TEXT_HTML_VALUE)
+		@ResponseBody
+		public String welcomeAsHTML2() throws IOException {
+			String blogUrl = "static/vn/index.html";
+			Document doc = Jsoup.connect(blogUrl).get();
+
+			Element link = new Element(Tag.valueOf("a"), "").text("Checkout this amazing website!")
+					.attr("href", "http://baeldung.com").attr("target", "_blank");
+			doc.appendChild(link);
+
+			return doc.toString();
+		}
+
+		@GetMapping(value = "/vn/welcome3", produces = MediaType.TEXT_HTML_VALUE)
+		@ResponseBody
+		public String welcomeAsHTML3() throws IOException {
+			// TODO:: URL 정책
+			Document doc = null;
+//	    	ClassPathResource resource = new ClassPathResource("static/vn/index.html");
+//	    	System.out.println("resource" + resource.toString());
+			try {
+//	    	    Path path = Paths.get(resource.getURI());
+//	    	    byte[] b = Files.readAllBytes(path);
+//	    	    System.out.println(b.length);
+//	    	    System.out.println(new String(b, Charset.forName("utf-8")));
+				doc = Jsoup.parse(vnHtml);
+				System.out.println(vnHtml.length());
+
+				Element link = new Element(Tag.valueOf("script"), "").text(
+						"window.__VP__ = { srcClientId: \"43a83638-0122-4a9f-baaa-a4b9eb3caa51\",srcDpaId: \"c21195dc-39eb-422c-8328-db2b0b535ed3\"}")
+						.appendText("aaa");
 //			    		  .attr("href", "http://baeldung.com")
 //			    		  .attr("target", "_blank");
-			    doc.body().appendChild(link);
-	    	} catch (IOException e) {
-	    		e.printStackTrace();
-	    	}
-	    	return doc.toString(); 
+				doc.body().appendChild(link);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return doc.toString();
 
-	    }
-	    
-	    @RequestMapping("/page")
-	    public String someOtherPage(HttpServletRequest request, HttpServletResponse response) {
-	        return "forward:/static/vn/index.html"; 
-	    }
-	    
+		}
+
+		@GetMapping(value = "/vn/welcome4", produces = MediaType.TEXT_HTML_VALUE)
+		@ResponseBody
+		public String welcomeAsHTML4() {
+			Document doc = null;
+			doc = Jsoup.parse(vnHtml);
+			System.out.println(vnHtml.length());
+
+			Element link = new Element(Tag.valueOf("script"), "")
+					.text("window.__VP__ = { srcClientId: \"43a83638-0122-4a9f-baaa-a4b9eb3caa51\",srcDpaId: \"c21195dc-39eb-422c-8328-db2b0b535ed3\"}");
+			
+			doc.body().appendChild(link);
+			return doc.toString();
+
+		}
+
+		@RequestMapping("/page")
+		public String someOtherPage(HttpServletRequest request, HttpServletResponse response) {
+			return "forward:/static/vn/index.html";
+		}
+
 		@RequestMapping("/home22")
 		public String home() {
 			return "forward:/static/test.html";
 		}
 
-	    
 	}
 
 }
